434bbdecfde49c5cceff0b842b1ae73621203a95
abegodong <abraham@upwardstech.com>
2016-02-20 08:25:12 +0700

    Update mysql.js
    change config error on load mysql

--- src/lib/cli/mysql.js
+++ src/lib/cli/mysql.js
@@ -2,11 +2,12 @@
  * Created by zaenal on 14/01/16.
  */
 var mysql = require('mysql');
-var config = require('config').db.mysql;
+process.env.NODE_CONFIG_DIR = '/config';
+var config = require('config');
 
 module.exports = mysql.createConnection({
-    host     : config.host,
-    user     : config.user,
-    password : config.password,
-    database : config.database
+    host     : config.get('db.mysql.host'),
+    user     : config.get('db.mysql.user'),
+    password : config.get('db.mysql.password'),
+    database : config.get('db.mysql.database')
 });
