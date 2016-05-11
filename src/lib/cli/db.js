9f0b6368a2f38ebca4ab383073174e7f77074ed7
zaenal <mzaenalm@gmail.com>
2016-05-11 15:03:42 +0700

    replicaset cli command

--- src/lib/cli/db.js
+++ src/lib/cli/db.js
@@ -3,12 +3,23 @@
  */
 var mongoose = require('mongoose');
 var utils = require('../utils'), config = utils.config();
+var _ = require('underscore');
 
 function db() {
 
     this.mongo = mongoose;
 
     this.dbUri = 'mongodb://' + config.get('db.mongo.host') + '/' + config.get('db.mongo.name');
+
+    var configDbMonggo = config.get('db.mongo');
+
+    if(_.isObject(configDbMonggo)){
+        this.dbUri = 'mongodb://' + config.get('db.mongo.host') + '/' + config.get('db.mongo.name');
+    } else if(_.isString(configDbMonggo)) {
+        this.dbUri = 'mongodb://' + configDbMonggo;
+    }
+
+    this.mongoOptions = config.get('db.mongo_options') || {};
 
     this.env = config.util.getEnv('NODE_ENV');
 
@@ -20,7 +31,7 @@ db.prototype.connect = function(){
 
     var me = this;
 
-    this.mongo.connect(me.dbUri);
+    this.mongo.connect(me.dbUri, me.mongoOptions);
 
     this.mongo.connection.once('open', function (callback) {
 
