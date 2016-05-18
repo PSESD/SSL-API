4b07ea31ffb31577353b40e9c126135222309ec9
zaenal <mzaenalm@gmail.com>
2016-05-18 18:31:15 +0700

    add depedency sshpass with sudo and add execute command

--- docker.sh
+++ docker.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-sshpass -p '$SSH_PASSWORD' ssh $SSH_USERNAME@$SSH_HOST
-cd /home/cbo/docker/api
-git pull origin develop
-docker rm -f api
-docker build -t psesd/ssl-api:develop .
-docker run -d --name api --link redis:redis -p 104.192.103.12:443:443 -e "NODE_ENV=development" -e "NODE_CONFIG_DIR=/config" -v /config:/config psesd/ssl-api:develop
+sshpass -p '$SSH_PASSWORD' ssh $SSH_USERNAME@$SSH_HOST "cd /home/cbo/docker/api && git pull origin develop && docker rm -f api && docker build -t psesd/ssl-api:develop . && docker run -d --name api --link redis:redis -p 104.192.103.12:443:443 -e NODE_ENV=development -e NODE_CONFIG_DIR=/config -v /config:/config psesd/ssl-api:develop"
