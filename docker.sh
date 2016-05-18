c02524161f7c5c38364ed029c3f4a6e9cb15700b
zaenal <mzaenalm@gmail.com>
2016-05-18 15:39:41 +0700

    push to serve with circleci

---
+++ docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ssh cbo@cbo.upward.st
+cd /home/cbo/docker/api
+git pull origin develop
+docker rm -f api
+docker build -t psesd/ssl-api:develop .
+docker run -d --name api --link redis:redis -p 104.192.103.12:443:443 -e "NODE_ENV=development" -e "NODE_CONFIG_DIR=/config" -v /config:/config psesd/ssl-api:develop
