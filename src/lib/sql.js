9b090aa2ec62ae37bd4ebc057f929474e0e7c7cb
Stephen <stephen.pugmire@itrellis.com>
2017-02-15 08:29:38 -0800

    Disabled MySQL connection

--- src/lib/sql.js
+++ src/lib/sql.js
@@ -8,9 +8,11 @@ var config = require('../lib/config').config();
 //console.log('NODE_CONFIG_DIR: ' + config.util.getEnv('NODE_CONFIG_DIR'));
 //console.log('NODE_APP_INSTANCE: ' + config.util.getEnv('NODE_APP_INSTANCE'));
 //console.log('ALLOW_CONFIG_MUTATIONS: ' + config.util.getEnv('ALLOW_CONFIG_MUTATIONS'));
+/*
 module.exports = mysql.createConnection({
     host     : config.get('MYSQL_HOST'),
     user     : config.get('MYSQL_USER'),
     password : config.get('MYSQL_PASSWORD'),
     database : config.get('MYSQL_DATABASE')
 });
+*/
