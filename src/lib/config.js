3f0b4c5b9d0365d0a294b600c9b6482758aebebe
mzily <margarett.ly@itrellis.com>
2017-02-16 09:19:37 -0800

    add line to ./lib/config.js to load environment variables on start

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,10 +1,11 @@
+require('dotenv').load()
 'use strict';
 
 module.exports.config = function () {
     var cfg = require('config');
     for (var prop in process.env) {
         if (cfg[prop] != process.env[prop]) {
-            cfg[prop] = process.env[prop]; 
+            cfg[prop] = process.env[prop];
         }
     }
     return cfg;
